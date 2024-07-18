8b9a2cc2b0ef7b7fe147012a80cc4d781a427fdc
少年老王 <zyk6271@gmail.com>
2024-07-18 10:59:23 +0800

    1.0.0
    1.change radio IQ invert

--- radio/radio_app.h
+++ radio/radio_app.h
@@ -29,7 +29,7 @@
 #define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
 #define LORA_SYMBOL_TIMEOUT                         0         // Symbols
 #define LORA_FIX_LENGTH_PAYLOAD_ON_DISABLE          false
-#define LORA_IQ_INVERSION_ON_DISABLE                false
+#define LORA_IQ_INVERSION_ON_DISABLE                true
 /*
  * Radio Config
  */
